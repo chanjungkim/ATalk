6cc8969a3dd1a19c66a1ed7497c5aa09c355c405
hongjunwan <dhks223@naver.com>
2017-08-31 15:37:38 +0900

    블랙리스트 추가 삭제 이벤트 등록
    다음은 데이터베이스랑 연결 시키기

--- src/chat/RoomList.java
+++ src/chat/RoomList.java
@@ -2,6 +2,7 @@ package chat;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,8 +13,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class RoomList extends JFrame{
+public class RoomList extends JFrame {
 	private JPanel panel;
 	private JPanel topPanel;
 	private JPanel listPanel;
@@ -21,17 +23,16 @@ public class RoomList extends JFrame{
 	private JButton roomList;
 	private JButton setting;
 	private JButton room;
-	
-	public RoomList(){
+
+	public RoomList() {
 		panel = new JPanel();
 		topPanel = new JPanel();
 		listPanel = new JPanel();
 		currentRoom = new JButton("현재 방");
-		roomList = new JButton("목록"); 
+		roomList = new JButton("목록");
 		setting = new JButton("설정");
 		room = new JButton("안드로이드 스터디 방");
 
-		
 		// Fucntion
 		room.addActionListener(new ActionListener() {
 			@Override
@@ -40,17 +41,19 @@ public class RoomList extends JFrame{
 				ChatClient chat = new ChatClient();
 			}
 		});
-		
+
+		////////////////////////////////////////////////////////////////////////
+		// 설정창 구현
 		setting.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Setting set = new Setting();
-				set.logoutBtn.addActionListener(new ActionListener() {
+				set.logoutBtn.addActionListener(new ActionListener() { // 로그아웃 버튼
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						int t = JOptionPane.showConfirmDialog(null, "이 계정을 로그아웃 하시겠습니까?","",JOptionPane.OK_CANCEL_OPTION,
-								JOptionPane.QUESTION_MESSAGE);
-						if(t==0) {
+						int t = JOptionPane.showConfirmDialog(null, "이 계정을 로그아웃 하시겠습니까?", "",
+								JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+						if (t == 0) {
 							System.out.println("Go to the previous: frame.");
 							set.dispose();
 							dispose();
@@ -58,29 +61,96 @@ public class RoomList extends JFrame{
 						}
 					}
 				});
+
+				set.blackBtn.addActionListener(new ActionListener() { // 블랙리스트 버튼
+					JFrame blackFm = new JFrame();
+					JPanel BtnPn = new JPanel();
+					JLabel nameLb = new JLabel("닉네임");
+					JTextField nameText = new JTextField(7);
+					JButton addBtn = new JButton("추가");
+					JButton delBtn = new JButton("삭제");
+					List nameList = new List();
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+
+						addBtn.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								String name = nameText.getText();
+								nameList.add(name);
+								nameText.setText(""); // 텍스트 필드내용 지우기
+								nameText.requestFocus(); // 텍스트 필드에 포커스 주기
+							}
+						});
+						delBtn.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								int pos = nameList.getSelectedIndex(); // namelist의 선택한 항목의 인덱스값 얻어오기
+
+								if (pos == -1) {
+									JOptionPane.showMessageDialog(blackFm, "선택해주세요!");
+									return;
+								}
+								nameList.remove(pos);
+							}
+						});
+
+						BtnPn.add(nameLb);
+						BtnPn.add(nameText);
+						BtnPn.add(addBtn);
+						BtnPn.add(delBtn);
+
+						blackFm.add(BtnPn, BorderLayout.NORTH);
+						blackFm.add(nameList, BorderLayout.CENTER);
+
+						blackFm.setTitle("블랙리스트");
+						blackFm.setSize(300, 400);
+						setDefaultCloseOperation(blackFm.EXIT_ON_CLOSE); // 이 창만 종료
+						blackFm.setVisible(true);
+					}
+				});
+
+				set.alarmBtn.addActionListener(new ActionListener() { // 알람 버튼
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+				set.colorBtn.addActionListener(new ActionListener() { // 색상 변경 버튼
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
 			}
 		});
-		
+		////////////////////////////////////////////////////////////////////////////////////
+
 		// Layout
 		panel.setLayout(new BoxLayout(panel, 1));
-		
-		// 
+
+		//
 		topPanel.add(roomList);
 		topPanel.add(currentRoom);
 		topPanel.add(setting);
-		listPanel.add(room);		
-		
+		listPanel.add(room);
+
 		panel.add(topPanel);
 		panel.add(listPanel);
-		
+
 		add(panel);
-		
+
 		setSize(700, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
-	public class Setting  extends JFrame{
+
+	public class Setting extends JFrame {
 		private JDialog set;
 		private JPanel alarmPn;
 		private JPanel backgroundPn;
@@ -92,7 +162,7 @@ public class RoomList extends JFrame{
 		private JButton logoutBtn;
 
 		public Setting() {
-			set = new JDialog(this,"설정");
+			set = new JDialog(this, "설정");
 			alarmPn = new JPanel();
 			backgroundPn = new JPanel();
 			alarmLb = new JLabel("알람");
@@ -102,7 +172,7 @@ public class RoomList extends JFrame{
 			blackBtn = new JButton("블랙리스트");
 			logoutBtn = new JButton("로그아웃");
 
-			set.setLayout(new GridLayout(4,1));
+			set.setLayout(new GridLayout(4, 1));
 			alarmPn.setLayout(new BorderLayout());
 			backgroundPn.setLayout(new BorderLayout());
 
@@ -110,13 +180,12 @@ public class RoomList extends JFrame{
 			alarmPn.add(alarmBtn, BorderLayout.EAST);
 			backgroundPn.add(backgroundLb, BorderLayout.WEST);
 			backgroundPn.add(colorBtn, BorderLayout.EAST);
-				
+
 			set.add(alarmPn);
 			set.add(backgroundPn);
 			set.add(blackBtn);
 			set.add(logoutBtn);
 
-			
 			set.setSize(200, 200);
 			set.setVisible(true);
 			setDefaultCloseOperation(set.EXIT_ON_CLOSE);
