d6c7bac6397759c1d06ca172ae6ef1704ac04f1d
jinyoung1105 <31239200+jinyoung1105@users.noreply.github.com>
2017-09-04 13:34:00 +0900

    컴파일
    Syso
    Scan 가능???

---
+++ code.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class code
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+int j = sc.nextInt();
+int z = sc.nextInt();
+
+System.out.println(i+j+z);
+	}
+}
