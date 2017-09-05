ba1988aa331129486be995109312b72605e72dec
odoria <31239288+odoria@users.noreply.github.com>
2017-09-05 09:10:34 +0900

    ;;

--- Code.java
+++ Code.java
@@ -4,6 +4,10 @@ import java.io.*;
 import java.util.Scanner;
 class Code{
 	public static void main (String[] args){
+<<<<<<< HEAD
 		System.out.println("dd");
+=======
+		System.out.println("hellllllllllll");
+>>>>>>> e0bfa98148d6ca17e1c8f33c126fb9be6dfffcf8
 	}
 }
