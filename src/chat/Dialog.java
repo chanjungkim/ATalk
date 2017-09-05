b91917593b55e3096948a06556d38441be89d392
odoria <31239288+odoria@users.noreply.github.com>
2017-09-05 17:16:46 +0900

    update

--- src/chat/Dialog.java
+++ src/chat/Dialog.java
@@ -4,66 +4,38 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Dialog {
 	JDialog imo;
-	JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+	JButton btn1;
 
 	public Dialog() {
+		JLabel lb1= new JLabel();
+		Icon icon=new ImageIcon("Penguins.jpg");
 		imo = new JDialog();
 		imo.setLayout(new GridLayout(0, 3));
 		btn1 = new JButton(new ImageIcon("Penguins.jpg"));
-		btn2 = new JButton("ff");
-		btn3 = new JButton("gg");
-		btn4 = new JButton("dd");
-		btn5 = new JButton("ff");
-		btn6 = new JButton("gg");
-		btn7 = new JButton("dd");
-		btn8 = new JButton("ff");
-		btn9 = new JButton("gg");
-		
-		
+
 		btn1.addActionListener(new ActionListener() {
-			
+
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-				
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btn1) {
+					
+
+				}
+
 			}
 		});
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		imo.add(btn1);
-		imo.add(btn2);
-		imo.add(btn3);
-		imo.add(btn4);
-		imo.add(btn5);
-		imo.add(btn6);
-		imo.add(btn7);
-		imo.add(btn8);
-		imo.add(btn9);
+
 		imo.setVisible(true);
 		imo.setSize(300, 300);
 		imo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
